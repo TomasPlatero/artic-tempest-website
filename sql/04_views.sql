b12e928695304e636df26f381975fb60f2d8e274
Tomás Platero <133709838+TomasPlatero@users.noreply.github.com>
2026-02-24 12:25:30 +0100

    Delete sql directory

--- sql/04_views.sql
+++
@@ -1,8 +0,0 @@
--- 04_views.sql
--- Vistas útiles
-
-create or replace view public.v_profiles_with_rank as
-select
-  p.*,
-  public.role_level_rank(p.role_level) as role_rank
-from public.profiles p;
