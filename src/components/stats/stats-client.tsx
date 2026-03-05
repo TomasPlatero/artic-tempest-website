ebc1eea06e147bf95d617ac514b25e21d6f3b7da
Tomás Platero <taplatero@outlook.es>
2026-03-05 01:13:27 +0100

    style: make WCL report modal wider and less tall

--- src/components/stats/stats-client.tsx
+++ src/components/stats/stats-client.tsx
@@ -456,7 +456,7 @@ export function StatsClient({ members, rioData, classColors = {} }: { members: a
 
                     {/* WCL REPORT MODAL */}
                     <Dialog open={!!selectedWclReport} onOpenChange={(open) => !open && setSelectedWclReport(null)}>
-                        <DialogContent className="max-w-2xl bg-[#0a0a0c] border-border/40 max-h-[90vh] flex flex-col p-0 overflow-hidden">
+                        <DialogContent className="max-w-5xl bg-[#0a0a0c] border-border/40 max-h-[70vh] flex flex-col p-0 overflow-hidden">
                             <DialogHeader className="p-6 pb-4 border-b border-border/10 bg-blue-500/5">
                                 <div className="flex items-center justify-between pr-4">
                                     <div>
