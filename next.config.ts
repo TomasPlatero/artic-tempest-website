e40deeb8c71ccaf372cc99ddb47a0534673c9f84
Tomás Platero <taplatero@outlook.es>
2026-08-07 09:14:44 +0200

    fix aikido firewall dependency

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,7 @@ const nextConfig: NextConfig = {
     "@radix-ui/react-context",
     "@radix-ui/react-direction",
     "@radix-ui/react-dismissable-layer",
+    "@aikidosec/firewall",
   ],
 
   allowedDevOrigins: [
